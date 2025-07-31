3b406c304d5a79f91ced1b988f0aae33e6cb566c
veravasilyeva <vera.vasilyeva@mail.ru>
2025-07-31 18:32:37 +0300

    Изменен формат вывода курса, 2 знака после запятой

--- Obmen.py
+++ Obmen.py
@@ -1,10 +1,28 @@
+from tkinter import *
+from tkinter import messagebox as mb
 import requests
 import json
-from tkinter import *
-from tkinter.ttk import meessagebox as mb
+
+def exchange():
+    code = entry.get()
+
+    if code:
+        try:
+            response = requests.get("https://open.er-api.com/v6/latest/USD")
+            response.raise_for_status()
+            data = response.json()
+            if code in data ['rates']:
+                exchange_rate = data ['rates'] [code]
+                mb.showinfo("Курс обмена", f"Курс к доллару: {exchange_rate:.2f} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание!", "Введите  код валюты")
 
 
-def
+
 
 window = Tk()
 window.title("Курсы обмена валют")
